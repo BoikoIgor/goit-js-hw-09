757ed59a817925955e21a754fbad300b893d8f1f
Igor Boiko <igorboiko@gmail.com>
2023-03-05 23:39:28 +0200

    add const to intervalId in 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -19,7 +19,7 @@ startBtn.addEventListener('click', () => {
   // Розблокуєм кнопку Stop
   stopBtn.disabled = false;
   // Запускаєм інтервал і записуємо його ідентифікатор. Інтервал викликатиме функцію changeBackgroundColor кожної секунди
-  intervalId = setInterval(changeBackgroundColor, 1000);
+  const intervalId = setInterval(changeBackgroundColor, 1000);
 });
 
 // Додаємо слухач на клік кнопки Stop
